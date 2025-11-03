c25011512b561d3c4908370907841bd9dd000d4c
steffen911 <steffen@langfuse.com>
2025-11-03 10:19:55 +0100

    chore: bump helm chart to 1.5.9

--- variables.tf
+++ variables.tf
@@ -66,7 +66,7 @@ variable "deletion_protection" {
 variable "langfuse_chart_version" {
   description = "Version of the Langfuse Helm chart to deploy"
   type        = string
-  default     = "1.5.8"
+  default     = "1.5.9"
 }
 
 variable "additional_env" {
